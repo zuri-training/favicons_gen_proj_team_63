4ae1bbc04769ff3c31b80ee045faaf9bad4c4b2d
Cofucan <coolucheofia@gmail.com>
2022-07-25 06:20:14 +0100

    connected to a PostgresSQL database

--- Zuricon/settings.py
+++ Zuricon/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Zuricon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ZuriconDB',
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
