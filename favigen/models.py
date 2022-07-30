c1c8b51e98395ec80a56a70a3f9bad461e412aaf
Cofucan <coolucheofia@gmail.com>
2022-07-30 23:01:01 +0100

    added custom user model

--- favigen/models.py
+++ favigen/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+from datetime import date
 
-# class CustomUser(AbstractUser):
-#     pass
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, blank=True, null=True, unique=True)
+    email = models.EmailField(_("email address"), unique=True)
+    # native_name = models.CharField(max_length=5)
+
+    USERNAME_FIELD = "email" # Making this the primary identifier
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    class Meta:
+        ordering =  ["email"] # The default ordering for the object, for use when obtaining lists of objects
+        # verbose_name = "User" # The name that shows up in the admin panel
+
+    def __str__(self):
+        return f"{self.email}"
