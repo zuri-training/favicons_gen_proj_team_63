e9838250b4102baeaeecfa5b6797e6a8040d4212
Cofucan <coolucheofia@gmail.com>
2022-07-31 22:12:08 +0100

    urls for home page and signup page

--- favigen/urls.py
+++ favigen/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home_page, name='home')
+    path("", views.home_page, name='home'),
+    path("signup/", views.signup, name='register')
 ]
