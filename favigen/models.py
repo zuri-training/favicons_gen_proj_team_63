d1276753dc34fdfe8bb4e0628be38a77b7861c7e
Cofucan <coolucheofia@gmail.com>
2022-07-31 01:51:44 +0100

    created custom user model with email as unique id

--- favigen/models.py
+++ favigen/models.py
@@ -1,21 +1,23 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
-from datetime import date
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
 
 
-class User(AbstractUser):
-    username = models.CharField(max_length=50, blank=True, null=True, unique=True)
-    email = models.EmailField(_("email address"), unique=True)
-    # native_name = models.CharField(max_length=5)
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
 
-    USERNAME_FIELD = "email" # Making this the primary identifier
-    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     class Meta:
-        ordering =  ["email"] # The default ordering for the object, for use when obtaining lists of objects
-        # verbose_name = "User" # The name that shows up in the admin panel
+        ordering = ['email']
+        verbose_name = 'User'
 
     def __str__(self):
-        return f"{self.email}"
+        return self.email
