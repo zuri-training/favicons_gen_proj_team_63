7c7367bc41790046298fd2c5b0c49a794f1f5e04
Uche Ofia <coolucheofia@gmail.com>
2022-08-05 23:42:45 +0100

    added django styling to form

--- favigen/forms.py
+++ favigen/forms.py
@@ -1,9 +1,38 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
+from django import forms
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+
+    # def __init__(self, *args: Any, **kwargs: Any) -> None:
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.fields["first_name"].widget.attrs.update({
+            'input type':'text',
+            'id': 'firstName',
+            'name': 'first_name',
+            'placeholder': 'First Name'
+        })
+        self.fields["email"].widget.attrs.update({
+            'input type':'email',
+            'id': 'email',
+            'name': 'email',
+            'placeholder': 'Email Address'
+        })
+        self.fields["password1"].widget.attrs.update({
+            'type':'password',
+            'id': 'password',
+            'name': 'password',
+            'placeholder': 'Password'
+        })
+        self.fields["password2"].widget.attrs.update({
+            'type':'password',
+            'id': 'password',
+            'name': 'password',
+            'placeholder': 'Password again'
+        })
+
 
     class Meta:
         model = CustomUser
