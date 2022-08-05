4b02172b002c9b2ba1ac988edbf60bdfb65cb362
Uche Ofia <66657791+Cofucan@users.noreply.github.com>
2022-08-05 13:35:57 +0100

    Revert "Deployment"

--- favigen/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home_page, name='home'),
-    path("signup/", views.signup, name='register'),
-    path("login/", views.login, name='login')
-]
