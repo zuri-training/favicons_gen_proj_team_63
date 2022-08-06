f671b14a8534dc1298aaa20514294f185b83339c
Umar_Farouk <yasmarfaq@yahoo.com>
2022-08-06 22:25:37 +0100

    added some changes

--- favigen/urls.py
+++ favigen/urls.py
@@ -9,7 +9,6 @@ app_name = "favigen"
 
 urlpatterns = [
     path("", views.home_page, name='home'),
-<<<<<<< HEAD
     path("signup/", views.signup, name='register'),
     path("login/", views.login, name='login'),
     path('upload/', views.upload,name='upload'),
@@ -18,8 +17,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
     path("signup/", views.signup_page, name='signup'),
     path("login/", views.login_page, name='login')
-]
->>>>>>> 0fb436d51a861efdb8fa5c00db68af6f5e1be7f2
+
