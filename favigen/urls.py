e538457fc7da10e6f034453616864721e650950f
Umar_Farouk <yasmarfaq@yahoo.com>
2022-08-06 20:39:25 +0100

    added url for upload view

--- favigen/urls.py
+++ favigen/urls.py
@@ -3,8 +3,11 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = "favigen"
+
 urlpatterns = [
     path("", views.home_page, name='home'),
+<<<<<<< HEAD
     path("signup/", views.signup, name='register'),
     path("login/", views.login, name='login'),
     path('upload/', views.upload,name='upload'),
@@ -13,3 +16,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+=======
+    path("signup/", views.signup_page, name='signup'),
+    path("login/", views.login_page, name='login')
+]
+>>>>>>> 0fb436d51a861efdb8fa5c00db68af6f5e1be7f2
