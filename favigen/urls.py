8896ca61a8e5b4fadf99193e606ea9e88d90942d
Cofucan <coolucheofia@gmail.com>
2022-08-07 11:31:05 +0100

    removed repeated code

--- favigen/urls.py
+++ favigen/urls.py
@@ -5,15 +5,12 @@ from django.conf.urls.static import static
 
 app_name = "favigen"
 
-app_name = "favigen"
-
 urlpatterns = [
     path("", views.home_page, name="home"),
     path("signup/", views.signup_page, name="register"),
     path("login/", views.login_page, name="login"),
     path("upload/", views.upload, name="upload"),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
