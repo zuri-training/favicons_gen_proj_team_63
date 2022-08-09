17a30bd3886f84afa0ba83ef682c3674f96c2b18
Uche Ofia <66657791+Cofucan@users.noreply.github.com>
2022-08-09 21:57:39 +0100

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = "favigen"
 
 urlpatterns = [
-    path("", views.home_page, name='home')
+    path("", views.home_page, name="home"),
+    path("signup/", views.signup_page, name="signup"),
+    path("login/", views.login_page, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("upload/", views.upload, name="upload"),
+    path("contact/", views.contact_page, name="contact"),
+    path("saved-icons/", views.saved_icons, name="saved"),
+    path("generated-icon/", views.generated_icon, name="generated"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # path("signup/", views.signup_page, name="signup"),
+    # path("login/", views.login_page, name="login")
