16fd8c3b95f7a0b938233d672d7d11fabd5eeca4
Uche Ofia <coolucheofia@gmail.com>
2022-08-15 13:55:07 +0100

    Dynamic rendering of saved icons

--- favigen/views.py
+++ favigen/views.py
@@ -183,12 +183,10 @@ def message_sent(request):
 def saved_icons(request):
     user = request.user
     current_user_email = user.email
-    favicons = Favicon.objects.get()
-
-    visitor = CustomUser.objects.get(email=current_user_email)
+    favicons = Favicon.objects.all()
 
     context = {'favicons': favicons}
-    return render(request, "favigen/saved-icons.html")
+    return render(request, "favigen/saved-icons.html", context)
 
 
 @login_required(login_url='fav:login')
